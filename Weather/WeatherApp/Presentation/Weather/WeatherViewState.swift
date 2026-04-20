7d6e09584effca020333e14f7f6f033f54058dea
albtugel <al.tugel02@gmail.com>
2026-04-21 02:28:53 +0500

    refactor: Weather UI

---
+++ Weather/WeatherApp/Presentation/Weather/WeatherViewState.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+struct WeatherViewState {
+    let backgroundConditionCode: Int
+    let locationText: String
+    let cityName: String
+    let temperatureText: String
+    let conditionText: String
+    let highLowText: String
+    let compactSummaryText: String
+    let currentWeather: MockWeatherData.Current
+    let hourlyItems: [MockWeatherData.HourlyWeather]
+    let dailyItems: [MockWeatherData.DailyWeather]
+}
